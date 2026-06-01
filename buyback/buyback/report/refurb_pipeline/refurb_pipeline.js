1d467426b52d68b5ff735631d9d5ec0afda67353
Pallavi Chandolia <pallavichandolia@congruenceholdings.in>
2026-06-01 14:47:50 +0000

    fix: set default company in refurb pipeline report

--- buyback/buyback/report/refurb_pipeline/refurb_pipeline.js
+++ buyback/buyback/report/refurb_pipeline/refurb_pipeline.js
@@ -2,7 +2,7 @@ frappe.query_reports["Refurb Pipeline"] = {
 	filters: [
 		{ fieldname: "from_date", label: __("From Date"), fieldtype: "Date", default: frappe.datetime.month_start() },
 		{ fieldname: "to_date", label: __("To Date"), fieldtype: "Date", default: frappe.datetime.now_date() },
-		{ fieldname: "company", label: __("Company"), fieldtype: "Link", options: "Company" },
+		{ fieldname: "company", label: __("Company"), fieldtype: "Link", options: "Company", default: frappe.defaults.get_user_default("Company") },
 		{ fieldname: "status", label: __("Status"), fieldtype: "Select", options: "\nReceived\nDiagnosed\nRepaired\nGraded\nRestocked\nCancelled" }
 	],
 };
